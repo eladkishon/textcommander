cb93b203f301f4814305ba7f7a44e08dfbba3f70
eladkishon <elad.kishon@gmail.com>
2024-12-29 17:07:27 -0500

    web netlify

---
+++ web2/app/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className='mt-24 flex h-32 flex-col items-center justify-between bg-gradient-to-b from-transparent to-primary/20'>
+            <div className='p-5 text-center'>
+                Built with ❤️ by Elad Kishon
+            </div>
+            <div className='mt-auto h-[1px] w-full bg-gradient-to-r from-transparent via-primary to-transparent'></div>
+        </footer>
+    )
+}
+
+export default Footer
