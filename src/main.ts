3c327b6856b6972986f17651a53468ddccee34a3
eladkishon <elad.kishon@gmail.com>
2025-01-01 19:35:46 -0500

    find unactive friends

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ChatCommanderBus } from "./commander"
 import { ChatCleanerPlugin } from "./plugins/chat_cleaner"
 import { EmergencyPlugin } from "./plugins/emergency"
+import { FriendsKeeperPlugin } from "./plugins/friendskeeper"
 import { getWhatsappClient } from "./whatsappclient"
 
 const client = getWhatsappClient()
 const bus = new ChatCommanderBus(client)
 bus.add_plugin(new EmergencyPlugin())
 bus.add_plugin(new ChatCleanerPlugin())
+bus.add_plugin(new FriendsKeeperPlugin())
 
 bus.start().then().catch(console.error)
--- src/plugins/friendskeeper.ts
+++ src/plugins/friendskeeper.ts
@@ -1,11 +1,52 @@
 import { Client, Message, Call } from "whatsapp-web.js";
 import { CommanderPlugin } from "../types";
-
+import schedule = require('node-schedule');
+import { differenceInDays, format } from "date-fns";
 
 
 export class FriendsKeeperPlugin implements CommanderPlugin {
-    init(client: Client): Promise<void> {
-        throw new Error("Method not implemented.");
+    client: Client;
+
+    private async checkForUnactiveFriends() {
+        const chats = await this.client.getChats()
+
+        const friends = chats.filter(c => !c.isGroup)
+
+        // for each friend, get messages, check there are more than 10 messages and the last message is older than 30 days
+        for (const friend of friends) {
+            const messages = await friend.fetchMessages({ limit: 50 })
+            if (messages.length < 10) {
+                continue
+            }
+
+
+            const daysDiff = differenceInDays(Date.now(), friend.lastMessage.timestamp * 1000)
+            
+            if (daysDiff > 30) {
+                console.log(`Friend ${friend.name} (having ${messages.length}) has not been active for ${daysDiff} days`)
+            }
+        }
+
+
+
+    }
+    async init(client: Client) {
+        this.client = client
+
+        // run now and every day same time
+        this.checkForUnactiveFriends()
+
+        // check for unactive friends every day using node-schedule
+        const rule = new schedule.RecurrenceRule();
+        const now = new Date();
+        rule.hour = now.getHours()
+        rule.minute = now.getMinutes()
+
+        schedule.scheduleJob(rule, async () => {
+            await this.checkForUnactiveFriends()
+        })
+
+
     }
     onMessage(msg: Message): Promise<void> {
         throw new Error("Method not implemented.");
