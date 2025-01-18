ae6876e13433ea98068d2330f451f503b2216ef3
Elad Kishon <elad.kishon@gmail.com>
2025-01-18 11:44:16 -0600

    rm ununsed

--- app/src/app/[locale]/(auth)/dashboard/page.tsx
+++ app/src/app/[locale]/(auth)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import { useTranslations } from 'next-intl';
 
-import { MessageState } from '@/features/dashboard/MessageState';
 import { TitleBar } from '@/features/dashboard/TitleBar';
-import { SponsorLogos } from '@/features/sponsors/SponsorLogos';
 
 const DashboardIndexPage = () => {
   const t = useTranslations('DashboardIndex');
